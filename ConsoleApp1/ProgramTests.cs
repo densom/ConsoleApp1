4eb7d478d10bddf87a7ff7e1e900b27b5b0e2f9f
Dennis Somerville <dennis.somerville@gmail.com>
2020-02-06 06:36:33 -0500

    before nukeeper

---
+++ ConsoleApp1/ProgramTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace ConsoleApp1.Tests
+{
+    [TestFixture]
+    public class ProgramTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Fail();
+        }
+    }
+
+
+
+}
